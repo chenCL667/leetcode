22e88b6d77bc105cd29b9f3ad71748736c40389c
chenCL667 <15810726658@163.com>
2020-03-07 20:38:56 +0800

    leetcode

---
+++ editor/en/[20]Valid Parentheses.java
@@ -0,0 +1,78 @@
+//Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+// determine if the input string is valid. 
+//
+// An input string is valid if: 
+//
+// 
+// Open brackets must be closed by the same type of brackets. 
+// Open brackets must be closed in the correct order. 
+// 
+//
+// Note that an empty string is also considered valid. 
+//
+// Example 1: 
+//
+// 
+//Input: "()"
+//Output: true
+// 
+//
+// Example 2: 
+//
+// 
+//Input: "()[]{}"
+//Output: true
+// 
+//
+// Example 3: 
+//
+// 
+//Input: "(]"
+//Output: false
+// 
+//
+// Example 4: 
+//
+// 
+//Input: "([)]"
+//Output: false
+// 
+//
+// Example 5: 
+//
+// 
+//Input: "{[]}"
+//Output: true
+// 
+// Related Topics String Stack
+
+
+import java.util.Stack;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        if (s.length() % 2 != 0) return false;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else {
+                char left;
+                if (!stack.empty()){
+                    left = stack.pop();
+                } else {
+                    return false;
+                }
+
+                if (left == '(' && s.charAt(i) != ')') return false;
+                if (left == '[' && s.charAt(i) != ']') return false;
+                if (left == '{' && s.charAt(i) != '}') return false;
+            }
+        }
+        if (!stack.empty()) return false;
+
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
